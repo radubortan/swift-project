5d90c183da4d656cd4bb8ce34d735ece31762b40
Radu Bortan <bortanradu@gmail.com>
2022-02-17 21:20:43 +0100

    Initial Commit

---
+++ PolyCook/PolyCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolyCookApp.swift
+//  PolyCook
+//
+//  Created by Radu Bortan on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PolyCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
