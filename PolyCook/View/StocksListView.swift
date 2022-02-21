7833e5f85ddbff3a3c83f425dead8f189431f6e6
Radu Bortan <bortanradu@gmail.com>
2022-02-21 13:00:32 +0100

    improved settings and added filter buttons

--- PolyCook/View/StocksListView.swift
+++ PolyCook/View/StocksListView.swift
@@ -19,80 +19,102 @@ struct StocksListView: View {
         NavigationView{
             VStack {
                 List {
-                    ForEach(searchResults, id: \.self) {ingredient in
-                        //version sans flèche
-                        //                        ZStack {
-                        //                            NavigationLink(destination: RecipeView()) {
-                        //                                    EmptyView()
-                        //                                }.opacity(0)
-                        //                            HStack {
-                        //                                Text(ingredient).font(.system(size: 21)).truncationMode(.tail)
-                        //                                Spacer()
-                        //                                Image(systemName: "exclamationmark.circle")
-                        //                                    .resizable()
-                        //                                    .frame(width: 25, height: 25)
-                        //                                    .foregroundColor(.red)
-                        //                                Text("10kg")
-                        //                                    .padding(10)
-                        //                                    .background(Color.stockAmountBackground)
-                        //                                    .cornerRadius(10)
-                        //                                    .foregroundColor(Color.textFieldForeground)
-                        //                            }.frame(height: 50)
-                        //                        }
-                        
-                        Button{
-                            showingSheet.toggle()
+                    Section {
+                        HStack() {
+                            Button ("Catégorie"){}
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .buttonStyle(BorderlessButtonStyle())
+                            
+                            Button ("Allergène"){}
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .buttonStyle(BorderlessButtonStyle())
                         }
-                        label : {
-                            HStack {
-                                Text(ingredient).font(.system(size: 21)).truncationMode(.tail).foregroundColor(.primary)
-                                Spacer()
-                                Image(systemName: "exclamationmark.circle")
-                                    .resizable()
-                                    .frame(width: 25, height: 25)
-                                    .foregroundColor(.red)
-//                                HStack (spacing: 5){
-//                                    Button {
-//
-//                                    } label: {
-//                                        Image(systemName: "minus.circle.fill").resizable().frame(width: 25, height: 25)
-//                                    }
-                                    Text("10kg")
-                                        .padding(10)
-                                        .background(Color.stockAmountBackground)
-                                        .cornerRadius(10)
-                                        .foregroundColor(Color.textFieldForeground)
-//                                    Button {
-//
-//                                    } label: {
-//                                        Image(systemName: "plus.circle.fill").resizable().frame(width: 25, height: 25)
-//                                    }
-//                                }
-                                
-                            }.frame(height: 50)
-                        }
-                        .sheet(isPresented : $showingSheet) {
-                            StockView()
-                        }
-                        
-                        //version avec flèche
-                        //                        NavigationLink(destination: RecipeView()) {
-                        //                            HStack {
-                        //                                Text(ingredient).font(.system(size: 21)).truncationMode(.tail)
-                        //                                Spacer()
-                        //                                Image(systemName: "exclamationmark.circle")
-                        //                                    .resizable()
-                        //                                    .frame(width: 25, height: 25)
-                        //                                    .foregroundColor(.red)
-                        //                                Text("10kg")
-                        //                                    .padding(10)
-                        //                                    .background(Color.stockAmountBackground)
-                        //                                    .cornerRadius(10)
-                        //                                    .foregroundColor(Color.textFieldForeground)
-                        //                            }
-                        //                        }.frame(height: 50)
+                        .listRowBackground(Color.white.opacity(0))
                     }
-//                    .listRowInsets(.init(top: 6, leading: 20, bottom: 6, trailing: 10))
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    
+                    Section {
+                        ForEach(searchResults, id: \.self) {ingredient in
+                            //version sans flèche
+                            //                        ZStack {
+                            //                            NavigationLink(destination: RecipeView()) {
+                            //                                    EmptyView()
+                            //                                }.opacity(0)
+                            //                            HStack {
+                            //                                Text(ingredient).font(.system(size: 21)).truncationMode(.tail)
+                            //                                Spacer()
+                            //                                Image(systemName: "exclamationmark.circle")
+                            //                                    .resizable()
+                            //                                    .frame(width: 25, height: 25)
+                            //                                    .foregroundColor(.red)
+                            //                                Text("10kg")
+                            //                                    .padding(10)
+                            //                                    .background(Color.stockAmountBackground)
+                            //                                    .cornerRadius(10)
+                            //                                    .foregroundColor(Color.textFieldForeground)
+                            //                            }.frame(height: 50)
+                            //                        }
+                            
+                            Button{
+                                showingSheet.toggle()
+                            }
+                            label : {
+                                HStack {
+                                    Text(ingredient).font(.system(size: 21)).truncationMode(.tail).foregroundColor(.primary)
+                                    Spacer()
+                                    Image(systemName: "exclamationmark.circle")
+                                        .resizable()
+                                        .frame(width: 25, height: 25)
+                                        .foregroundColor(.red)
+    //                                HStack (spacing: 5){
+    //                                    Button {
+    //
+    //                                    } label: {
+    //                                        Image(systemName: "minus.circle.fill").resizable().frame(width: 25, height: 25)
+    //                                    }
+                                        Text("10kg")
+                                            .padding(10)
+                                            .background(Color.stockAmountBackground)
+                                            .cornerRadius(10)
+                                            .foregroundColor(Color.textFieldForeground)
+    //                                    Button {
+    //
+    //                                    } label: {
+    //                                        Image(systemName: "plus.circle.fill").resizable().frame(width: 25, height: 25)
+    //                                    }
+    //                                }
+                                    
+                                }.frame(height: 50)
+                            }
+                            .sheet(isPresented : $showingSheet) {
+                                StockView()
+                            }
+                            
+                            //version avec flèche
+                            //                        NavigationLink(destination: RecipeView()) {
+                            //                            HStack {
+                            //                                Text(ingredient).font(.system(size: 21)).truncationMode(.tail)
+                            //                                Spacer()
+                            //                                Image(systemName: "exclamationmark.circle")
+                            //                                    .resizable()
+                            //                                    .frame(width: 25, height: 25)
+                            //                                    .foregroundColor(.red)
+                            //                                Text("10kg")
+                            //                                    .padding(10)
+                            //                                    .background(Color.stockAmountBackground)
+                            //                                    .cornerRadius(10)
+                            //                                    .foregroundColor(Color.textFieldForeground)
+                            //                            }
+                            //                        }.frame(height: 50)
+                        }
+    //                    .listRowInsets(.init(top: 6, leading: 20, bottom: 6, trailing: 10))
+                    }
                 }
                 .searchable(text: $enteredText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Recherche ingrédient")
                 .navigationTitle("Stocks")
